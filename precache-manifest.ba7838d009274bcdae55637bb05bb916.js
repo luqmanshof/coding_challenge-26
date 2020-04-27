a8e50e94ab0be8153dcb2a17d3e8005f50bffc94
Luqman Shofuleji <luqmanshof@gmail.com>
2020-04-27 20:58:09 +0100

    Updates

---
+++ precache-manifest.ba7838d009274bcdae55637bb05bb916.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e07e6064682fe63263b1143b063721f5",
+    "url": "/coding_challenge-26/index.html"
+  },
+  {
+    "revision": "c56dde93b152a9e3e729",
+    "url": "/coding_challenge-26/static/css/main.3d5ba3d4.chunk.css"
+  },
+  {
+    "revision": "f3f3d4636f7c5f2215f7",
+    "url": "/coding_challenge-26/static/js/2.adf986ef.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coding_challenge-26/static/js/2.adf986ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c56dde93b152a9e3e729",
+    "url": "/coding_challenge-26/static/js/main.8a399641.chunk.js"
+  },
+  {
+    "revision": "15e377badc32e3b3f9d9",
+    "url": "/coding_challenge-26/static/js/runtime-main.357c7d5a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coding_challenge-26/static/media/image.d7265326.png"
+  }
+]);
